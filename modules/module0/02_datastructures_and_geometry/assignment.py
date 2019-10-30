1a7ac82211ab88d9e65f26b1b534672e45855c7c
tetov <a@tetov.se>
2019-10-30 01:06:32 +0100

    Assignment deadline

--- modules/module0/02_datastructures_and_geometry/assignment.py
+++ modules/module0/02_datastructures_and_geometry/assignment.py
@@ -8,9 +8,15 @@ from typing import List, Iterator
 
 import numpy as np
 
+from compas import get
+from compas.datastructures import Mesh
+
 
 def vector_2pt_xy(point1: List[float], point2: List[float]) -> List[float]:
     ''' Returns vector between two points
+
+        >>> vector_2pt_xy([-1, -4, 8], [2, 9, 2])
+        [3.0, 13.0, -6.0]
     '''
 
     if len(point1) != len(point2):
@@ -35,6 +41,8 @@ def flip_matrix(matrix: List) -> Iterator:
 
 
 def midpoint_convex_polygon(verts: List[List[float]]) -> List[float]:
+    ''' Find polygon centroid given a 2d convex polygon defined by list of vertices
+    '''
 
     flipped_verts = flip_matrix(verts)
 
@@ -103,7 +111,7 @@ def normalize_vector(v: List[float]) -> List[float]:
 
 
 def orthonormal_vectors(vector1: List[float], vector2: List[float]) -> List[List[float]]:
-    ''' Task 1
+    ''' Geometry task 1
         Returns orthonormal basis given two vectors
 
 
@@ -121,7 +129,7 @@ def orthonormal_vectors(vector1: List[float], vector2: List[float]) -> List[List
 
 
 def area_convex_polygon(verts: List[List[float]]) -> float:
-    ''' Task 2
+    ''' Geometry task 2
         Returns area of convex polygon defined by corner points
         >>>
     '''
@@ -149,8 +157,11 @@ def area_convex_polygon(verts: List[List[float]]) -> float:
     return sum(areas)
 
 
-def task3_wo_numpy(vectors1: List[float], vectors2: List[float]) -> List[float]:
-    ''' Task 3a
+def task3_wo_numpy(vectors1: List[float], vectors2: List[float]) -> List[List[float]]:
+    ''' Geometry task 3a
+
+        >>> task3_wo_numpy([[2, 5, -1], [12, 15, 0], [9, 8, 1]], [[ -12, -4, 1], [9, 4, 1], [0, 5, 2]])
+        [[1, 10, 52], [15, -12, -87], [11, -18, 45]]
 
     '''
 
@@ -163,16 +174,65 @@ def task3_wo_numpy(vectors1: List[float], vectors2: List[float]) -> List[float]:
 
 
 def task3_w_numpy(vectors1: List[float], vectors2: List[float]) -> List[float]:
-    ''' Task 3b
+    ''' Geometry task 3b
+
+        >>> task3_w_numpy([[2, 5, -1], [12, 15, 0], [9, 8, 1]], [[ -12, -4, 1], [9, 4, 1], [0, 5, 2]])
+        array([[  1,  10,  52],
+               [ 15, -12, -87],
+               [ 11, -18,  45]])
 
     '''
 
     v1_array = np.array(vectors1)
     v2_array = np.array(vectors2)
 
-    return cross_product(v1_array, v2_array)
+    return np.cross(v1_array, v2_array)
+
+
+def traverse_mesh() -> None:
+    mesh = Mesh.from_obj(get('faces.obj'))
+
+    # get dict with keys and coords
+    vert_coords = {}
+    for vkey in mesh.vertices():
+        vert_coords[vkey] = mesh.vertex_coordinates(vkey)
+
+    # right hand side of mesh
+    start_vert = vert_coords.pop(17)
+
+    print('Starting traversal\nStart vertex: {:0f}, {:0f}, {:0f}'.format(*start_vert))
+
+    # setup goal x value
+    x_values = []
+    for key in vert_coords:
+        x_values.append(vert_coords[key][0])
+
+    x_goal = min(x_values)
+
+    while start_vert[0] != x_goal:
+        # find magnitudes of vectors between vertices and start
+        magnitudes = {}
+        for key in vert_coords:
+            # TODO: Remove points from dict if x < start_x
+            if vert_coords[key][0] < start_vert[0]:
+                vector = vector_2pt_xy(start_vert, vert_coords[key])
+                magnitude = magnitude_vector(vector)
+                magnitudes[key] = magnitude
+
+        key_min = min(magnitudes.keys(), key=(lambda k: magnitudes[k]))
+
+        start_vert = vert_coords.pop(key_min)
+
+        print('Found vertex at {:0f}, {:0f}, {:0f}'.format(*start_vert))
+
+    print('Mesh traversed')
 
 
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
+    print("Module1 assignment by " + __author__ + "\n")
+    print("The geometry tasks are demonstrated through their doctests.")
+    print("The datastructures task is demonstrated using console output.")
+
+    traverse_mesh()
